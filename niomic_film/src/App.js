deecb7fd7908a2e50c36d719bd6f30501a52aad7
Roisro <roisro35@gmail.com>
2021-11-28 15:32:01 +0700

    Pert 1-2

---
+++ niomic_film/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div>
+      <p> Home</p>
+      <Button primary> Test Semantic UI</Button>
+    </div>
+  )
+}
+
+export default App
