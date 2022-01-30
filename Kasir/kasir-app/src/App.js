d6abead13ab844392a874dc94fa5e4486ab82961
Roisro <roisro35@gmail.com>
2022-01-31 00:15:31 +0700

    Install React JS, Json Placeholder, Setting Font dan Layout

---
+++ Kasir/kasir-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Hasil, ListCategories, NavbarComponent} from "./components"
+import { Row, Col, Container } from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+      
+        <NavbarComponent />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <ListCategories />
+              <Col>
+                <h4><strong> Daftar Produk </strong></h4>
+                <hr />
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
+    </div>
+  );
+}
+
+export default App;
