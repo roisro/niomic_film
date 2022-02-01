86ea37a7d02b8270dabafb64a22b87865895b03f
Roisro <roisro35@gmail.com>
2022-02-01 11:55:05 +0700

    pert. 5

--- Kasir/kasir-app/src/App.js
+++ Kasir/kasir-app/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react'
-import { Hasil, ListCategories, NavbarComponent} from "./components"
+import React, { Component } from 'react';
+import { Hasil, ListCategories, NavbarComponent, Menus} from "./components"
 import { Row, Col, Container } from 'react-bootstrap'
+import { API_URL } from './utils/constants'
+import axios from 'axios'
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       menus : []
+    }
+  }
+
+  componentDidMount () {
+    axios
+      .get(API_URL+"products")
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch (error => {
+        console.log(" Error ni yak",error)
+      })
+  }
+
+
+  render() {
+    const {menus} = this.state
+    return (
     <div className="App">
-      
-        <NavbarComponent />
-        <div className="mt-3">
-          <Container fluid>
-            <Row>
-              <ListCategories />
-              <Col>
-                <h4><strong> Daftar Produk </strong></h4>
-                <hr />
-              </Col>
-              <Hasil />
-            </Row>
-          </Container>
-        </div>
+      <NavbarComponent />
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong> Daftar Produk </strong>
+              </h4>
+              <hr />
+              <Row>
+                {menus  && menus.map((menu) => (
+                  <Menus 
+                      key={menu.id}
+                      menu={menu}
+                  />
+                ))}
+              </Row>
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
-  );
+    )
+  }
 }
+
 
 export default App;
